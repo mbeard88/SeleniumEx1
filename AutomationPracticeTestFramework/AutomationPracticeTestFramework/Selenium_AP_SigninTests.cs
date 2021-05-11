9b4f310e00a984f60b101bda643205ad94d49ba2
Martin <mbeard@spartaglobal.com>
2021-05-11 14:06:33 +0100

    First Commit

---
+++ AutomationPracticeTestFramework/AutomationPracticeTestFramework/Selenium_AP_SigninTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using NUnit.Framework;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AutomationPracticeTestFramework
+{
+    public class Selenium_AP_SigninTests
+    {
+
+        [Test()]
+        public void GivenIAmOnTheHomePage_WhenIClickTheSignPage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Manage().Window.Maximize();
+
+                driver.Navigate().GoToUrl("http://automationpractice.com/");
+
+                IWebElement signinLink = driver.FindElement(By.LinkText("Sign in"));
+
+                signinLink.Click();
+                Thread.Sleep(1000);
+
+                Assert.That(driver.Title, Is.EqualTo("Login - My Store"));
+            }
+
+        }
+        [TestCase("blahblah")]
+        [TestCase("four")]
+        public void GivenIAmOnTheSignInPage_AndIEnterA4DigitPassword_WhenIClickTheSigninButton_ThenIGetAnErrorMessage(string p)
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Manage().Window.Maximize();
+
+                driver.Navigate().GoToUrl("http://automationpractice.com/index.php?controller=authentication&back=my-account");
+
+                IWebElement emailField = driver.FindElement(By.Id("email"));
+
+                emailField.SendKeys("testing@naughtymail.com");
+
+                IWebElement passwordField = driver.FindElement(By.Id("passwd"));
+
+                passwordField.SendKeys(p);
+
+                IWebElement signinLink = driver.FindElement(By.Id("SubmitLogin"));
+
+                signinLink.Click();
+
+                Thread.Sleep(1000);
+
+                IWebElement signinMessage = driver.FindElement(By.ClassName("alert"));
+
+
+                if (p == "four")
+                {
+                    Assert.That(signinMessage.Text.Contains("Invalid password"));
+                }
+                else
+                {
+                    Assert.That(signinMessage.Text.Contains("Authentication failed"));
+                }
+
+            }
+
+
+        }
+    }
+}
